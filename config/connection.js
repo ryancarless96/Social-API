233be1f1bdfe4a4250d07353f0d5df54456a4201
Ryan Carless <ryancarless96@gmail.com>
2023-02-14 16:35:25 -0500

    commit changes

---
+++ config/connection.js
@@ -0,0 +1,11 @@
+const {connect, connection} = require('mongoose');
+
+const connectionString = 
+process.env.MONGODB_URI || '';
+
+connect(connectionString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+module.exports = connection;
