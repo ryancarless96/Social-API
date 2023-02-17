50fd51ea245f9150c66ac11a592f139e228b8d70
Ryan Carless <ryancarless96@gmail.com>
2023-02-17 16:03:17 -0500

    commit changes

--- controllers/networkController.js
+++ controllers/networkController.js
@@ -1,51 +1,51 @@
-const { Network, User } = require('../models');
+const { Reaction, User } = require('../models');
 
 module.exports = {
     getNetworks(req, res) {
-        Network.find()
-            .then((networks) => res.json(networks))
+        Reaction.find()
+            .then((reactions) => res.json(reactions))
             .catch((err) => res.status(500).json(err));
     },
-    getSingleNetwork(req, res) {
-        Network.findOne({ _id: req.params.networkId })
+    getSingleReaction(req, res) {
+        Reaction.findOne({ _id: req.params.networkId })
             .select('-_v')
-            .then((network) =>
-                !network
-                    ? res.status(404).json({ message: 'No network with that ID' })
-                    : res, json(network)
+            .then((reaction) =>
+                !reaction
+                    ? res.status(404).json({ message: 'No reaction with that ID' })
+                    : res, json(reaction)
             )
             .catch((err) => res.status(500).json(err));
     },
 
-    createNetwork(req, res) {
-        Network.create(req.body)
-            .then((network) => res.json(network))
+    createReaction(req, res) {
+        Reaction.create(req.body)
+            .then((reaction) => res.json(reaction))
             .catch((err) => {
                 console.log(err);
                 return res.status(500).json(err);
             });
     },
 
-    deleteNetwork(req, res) {
-        Network.findOneAndDelete({ _id: req.params.networkId })
-            .then((network) =>
-                !network
-                    ? res.status(404).json({ message: 'No network with that ID' })
-                    : User.deleteMany({ _id: { $in: network.users } })
+    deleteReaction(req, res) {
+        Reaction.findOneAndDelete({ _id: req.params.networkId })
+            .then((reaction) =>
+                !reaction
+                    ? res.status(404).json({ message: 'No reaction with that ID' })
+                    : User.deleteMany({ _id: { $in: reaction.users } })
             )
-            .then(()=> res.json({message: 'Network and users deleted!'}))
+            .then(()=> res.json({message: 'Reaction and users deleted!'}))
             .catch((err)=> res.status(500).json(err));
     },
-    updateNetwork(req,res) {
-        Network.findOneAndUpdate(
-            {_id: req.params.networkId},
+    updateReaction(req,res) {
+        Reaction.findOneAndUpdate(
+            {_id: req.params.reactionId},
             {$set: req.body},
             {runValidators: true, new: true}
         )
-        .then((network)=> 
-        !network
-        ? res.status(404).json({message: 'No network that id!'})
-        : res.json(network)
+        .then((reaction)=> 
+        !reaction
+        ? res.status(404).json({message: 'No reaction that id!'})
+        : res.json(reaction)
         )
         .catch((err)=> res.status(500).json(err));
     },
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,38 +1,19 @@
 const {User, Thought, Reaction} = require('../models');
 
 module.exports = {
-    getThoughts(req,res)  {
-        Thought.find()
-        .then((thoughts) => res.json(thoughts))
-        .catch((err)=> res.status(500).json(err));
-    },
-    getSingleThought(req,res) {
-        Thought.findOne({_id: req.params.thoughtId})
-        .select('-_v')
-        .then((thought) => 
-        !thought  
-        ? res.status(404).json({message: 'No thought with that ID'})
-        : res.json(thought)
-    )
-    .catch((err)=> res.status(500).json(err));
-    },
-    createThought(req,res) {
-        Thought.create(req.body)
-        .then((thought)=> res.json(thought))
+    getUsers(req,res)  {
+        User.find()
+        .then(async(users)=> {
+            const usersObj = {
+                users,
+                headCount: await headCount(),
+            };
+            return res.json(usersObj);
+        })
         .catch((err)=> {
             console.log(err);
-            return res.status(500).json(err); 
+            return res.status(500).json(err);
         });
     },
-
-    deleteThought(req,res) {
-        Thought.findOneAndDelete({_id: req.params.thoughtId})
-        .then((thought) => 
-        !thought
-        ? res.status(404).json({message: 'No thought with that ID'})
-        : Network.findOneAndUpdate(
-            {}
-        )
-        )
-    }
+    
 }
